d0854699df7ff5b04b167b8005ca0c38c8f7c55d
Sean Shubin <sshubin@cj.com>
2018-01-04 16:36:52 -0800

    handle case of already in current directory

--- domain/src/main/scala/com/seanshubin/project/generator/domain/CommandLineRunner.scala
+++ domain/src/main/scala/com/seanshubin/project/generator/domain/CommandLineRunner.scala
@@ -9,9 +9,19 @@ class CommandLineRunner(commandLineArguments: Array[String],
                         createSpecificationRunner: (Project, Path) => Runnable) extends Runnable {
   override def run(): Unit = {
     val configurationFilePath = Paths.get(commandLineArguments(0))
-    val destinationDirectory = configurationFilePath.getParent
+    val destinationDirectory = getParent(configurationFilePath)
     val projectSpecification = specificationLoader.load(configurationFilePath)
     val runner = createSpecificationRunner(projectSpecification, destinationDirectory)
     runner.run()
+  }
+
+  private def getParent(path: Path): Path = {
+    val parent = path.getParent
+    if (parent == null) {
+      val absoluteParent = path.toAbsolutePath.getParent
+      absoluteParent
+    } else {
+      parent
+    }
   }
 }
